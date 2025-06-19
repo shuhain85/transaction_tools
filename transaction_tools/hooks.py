3221a79d9addf2b5aebf8add3bb26a0c351b7953
shuhain85 <shuhain.ismail@gmail.com>
2025-06-19 21:13:25 +0500

    Initial commit

--- transaction_tools/hooks.py
+++ transaction_tools/hooks.py
@@ -8,6 +8,7 @@ app_license = "mit"
 
 # Includes in <head>
 # ------------------
+fixtures = ["Workspace"]
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/transaction_tools/css/transaction_tools.css"
---
+++ transaction_tools/transaction_tools/doctype/transaction_deletion_record_details/transaction_deletion_record_details.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024, Your Company and Contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+class TransactionDeletionRecordDetails(Document):
+    pass
